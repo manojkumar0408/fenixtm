ac05f33412a69d5772053882b51732e3ce1aee3b
fenixtm <walerson.zoin@gmail.com>
2017-05-28 12:43:18 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,1 +1,11 @@
+#'/bin/bash
+#Script de instalaçao automatica Minegate-cli
+#Feito Por FENIX
+pacote=$(dpkg --get-selections | grep minergate-cli  ) 
+if [ -n "$pacote" ] ; then 
+echo "O minergate-cli ja esta instalado"
+else
+echo "O MINERGATE-CLI NAO ESTA INSTALADO"
+echo "Iniciando Instalaçao"
 sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-cli.deb && sudo dpkg -i minergate-cli.deb
+fi
