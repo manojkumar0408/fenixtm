40a037a389a127f5de33a6ffd16b7b14abfae000
FENIX TM SCRIPTS <walerson.zoin@gmail.com>
2017-07-04 13:29:01 -0300

    Update install.bash

--- install.bash
+++ install.bash
@@ -16,7 +16,6 @@ ARCH=$(uname -m)
 if [[ "$OS" = "Ubuntu" || ver="12.04" || ver="14.04" || ver="16.04"]] ; then 
     pacote=$(dpkg --get-selections | grep minergate-cli  ) 
 cores=$(grep "model name" /proc/cpuinfo | wc -l )
-if [ARCH = i686 ] ; then 
 if [ -n "$pacote" ] ; then 
 echo -e "\n \033[01;32;40m O minergate-cli ja esta instalado \033[01;0m \n"
 echo -e "\033[01;34m  1: Crei uma conta em \033[01;31;40m https://minergate.com/ \033[00;0m \n"
@@ -55,7 +54,6 @@ sudo apt-get update && wget https://minergate.com/download/deb-cli -O minergate-
 echo -e "\033[00;0m"
 rm minergate-cli.deb
 bash <(curl -L -Ss https://raw.githubusercontent.com/fenixtm/MINERGATE-CLI/master/install.bash)
-fi
 fi
 else
     echo -e "\033\[01;41m SEU SISTEMA NAO ACEITA ESTA INSTAÇAO \033\[00;01m" 
